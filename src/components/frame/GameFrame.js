2a744af3bdafdaa9052f65b8f57a69582ad063f2
randmass <rand.mass9@gmail.com>
2022-09-11 18:25:24 +0300

    random start

--- src/components/frame/GameFrame.js
+++ src/components/frame/GameFrame.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./frameStyle.css";
+let rowRand = Math.floor(Math.random()*4);
+let colRand = Math.floor(Math.random()*4);
 
 let numbers = [
   [0, 0, 0, 0],
-  [0, 0, 3, 0],
-  [0, 0, 2, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
   [0, 0, 0, 0],
 ];
-
+numbers[rowRand][colRand]=2;
 function GameFrame() {
   return (
     <div className="gameFrame">
       {numbers.map((row) =>
         row.map((number) => (
           <div className="cell">
-            <label className="cellNumber">{number}</label>
+            <label className="cellNumber">{number>0 ? number : null}</label>
           </div>
         ))
       )}
