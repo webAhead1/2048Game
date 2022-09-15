98eabccdb1e3863a366a01df96982cd71113f546
Abdallah <abdlaa1912@gmail.com>
2022-09-15 20:00:22 +0300

    added game state message and made game box transparent

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -39,7 +39,8 @@ function GameFrame(props) {
       }
     };
 
-    window.addEventListener("keydown", handleKeyDown);
+    if (props.gameState === "inProgress")
+      window.addEventListener("keydown", handleKeyDown);
 
     const cleanup = () => {
       window.removeEventListener("keydown", handleKeyDown);
@@ -47,19 +48,26 @@ function GameFrame(props) {
     return cleanup;
   });
 
-  return props.gameState === "gameWon" ? (
-    <Message style="winMessage">Game Won</Message>
-  ) : props.gameState === "gameOver" ? (
-    <Message style="loseMessage">Game Over</Message>
-  ) : (
-    <div className="gameFrame">
-      {props.numbers.map((row) =>
-        row.map((number) => (
-          <div className="cell" key={cellId++}>
-            <label className="cellNumber">{number > 0 ? number : null}</label>
-          </div>
-        ))
-      )}
+  return (
+    <div className="container">
+      {props.gameState == "gameWon" ? (
+        <Message style="winMessage">Game Won</Message>
+      ) : props.gameState == "gameOver" ? (
+        <Message style="loseMessage">Game Over</Message>
+      ) : null}
+      <div className={props.gameState === "inProgress" ? "show" : "hide"}>
+        <div className="gameFrame">
+          {props.numbers.map((row) =>
+            row.map((number) => (
+              <div className="cell" key={cellId++}>
+                <label className="cellNumber">
+                  {number > 0 ? number : null}
+                </label>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 }
