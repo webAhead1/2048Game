79ed9926558241a05bc03bd5cfbdb5125050f755
Abdallah <abdlaa1912@gmail.com>
2022-09-15 11:47:06 +0300

    applied style updates

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from "react";
+import Game from "./components/game/Game";
+import Heading from "./components/Heading/Heading";
+import LogOut from "./components/LogOut/LogOut";
+
+function App() {
+  const [numbers, updateNumbers] = React.useState([]);
+  return (
+    <main className="App">
+      <Heading />
+
+      <Game numbers={numbers} updateNumbers={updateNumbers} />
+      <LogOut />
+    </main>
+  );
+}
+
+export default App;
--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./Message.scss";
 
-function Message(props){
-    return(
-    <h1 className="message">{props.Message}</h1>   
-    );
+function Message(props) {
+  return <h1 className={props.style}>{props.children}</h1>;
 }
 export default Message;
